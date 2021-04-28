b738c1864262c86c32c2c7b4303c3d3cfc743f0e
Ethan Vesely <Evesel259@wes-mec.org>
2021-04-27 18:16:37 -0700

    finished the home page and formatted it

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var slideNumber = 1;
+showSlides(slideNumber);
+
+function changeSlide(n) {
+    showSlides(slideNumber += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideNumber = n);
+}
+
+function showSlides(n) { //This is used to change the image being shown in the slideshow. It hides the old image and displays a new one when the arrow is pressed. 
+    var i = 0;
+    var slides = document.getElementsByClassName('slide');
+    if (n > slides.length) {
+        slideNumber = 1;
+    }
+    if (n < 1) {
+        slideNumber = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideNumber-1].style.display = 'block';
+}
